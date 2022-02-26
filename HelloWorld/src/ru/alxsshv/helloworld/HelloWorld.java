f2899ac746ef4e539fbe22976ac6fd1f73d52ce9
alxsshv <alxsshv@gmail.com>
2022-02-26 22:07:14 +0300

    lessons
    v1.8

---
+++ HelloWorld/src/ru/alxsshv/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.alxsshv.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int i = 6;
+		int j = 7;
+		int answer;
+		answer = i * j;
+		String message1 = "Hello";
+		String message2 = " world";
+		System.out.println(i);
+		System.out.println(message1 + message2);
+		System.out.println(answer);
+	}
+	
+}
