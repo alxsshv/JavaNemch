d7250b2dc2ba56124db6581812919cf27e3191e8
alxsshv <alxsshv@gmail.com>
2022-02-27 23:38:11 +0300

    v2.3

---
+++ CarManager/src/ru/alxsshv/carmanager/Car.java
@@ -0,0 +1,19 @@
+package ru.alxsshv.carmanager;
+
+public class Car {
+
+	String name;
+	int yearOfProduction;
+	int price;
+	int weight;
+	String color;
+	
+	public Car(String name, int yearOfProduction, int price, int weight, String color) {
+		this.name = name;
+		this.yearOfProduction = yearOfProduction;
+		this.price = price;
+		this.weight = weight;
+		this.color = color;
+	}
+	
+}
