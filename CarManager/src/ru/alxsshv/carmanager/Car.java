456c4cfe097d197faff6e6efb6d3eaff960c25b7
alxsshv <alxsshv@gmail.com>
2022-03-01 22:51:37 +0300

    v 2.7

--- CarManager/src/ru/alxsshv/carmanager/Car.java
+++ CarManager/src/ru/alxsshv/carmanager/Car.java
@@ -1,5 +1,7 @@
 package ru.alxsshv.carmanager;
 
+import java.util.Objects;
+
 public class Car {
 
 	String name;
@@ -7,6 +9,7 @@ public class Car {
 	int price;
 	int weight;
 	Color color;
+	private int	distance = 0;
 	
 	public Car(String name, int yearOfProduction, int price, int weight, Color color) {
 		this.name = name;
@@ -15,11 +18,37 @@ public class Car {
 		this.weight = weight;
 		this.color = color;
 	}
+	
+	public void addDistance (int additionalDistance) {
+		distance += additionalDistance;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
 
 	@Override
 	public String toString() {
 		return "Car [name=" + name + ", yearOfProduction=" + yearOfProduction + ", price=" + price + ", weight="
-				+ weight + ", color=" + color + "]";
+				+ weight + ", color=" + color + ", distance=" + distance + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(color, name, price, weight, yearOfProduction);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		return color == other.color && Objects.equals(name, other.name) && price == other.price
+				&& weight == other.weight && yearOfProduction == other.yearOfProduction;
 	}
 	
 }
--- CarManager/src/ru/alxsshv/carmanager/CarManager.java
+++ CarManager/src/ru/alxsshv/carmanager/CarManager.java
@@ -8,8 +8,14 @@ public class CarManager {
 		
 		Car renault = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 		
+		Car renault2 = new Car("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
+		
+		prius.addDistance(1000);
+		prius.addDistance(1000);
 		
 		System.out.println(prius);
+		
+		System.out.println(renault.equals(renault2));
 		}
 	
 
